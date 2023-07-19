bdd213f70398b34e691e583910af74f12301bdc4
Fordjour12 <fordjourbobiem@gmail.com>
2023-07-19 17:44:55 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,183 @@
+// import "./style.css";
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.js'
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
+
+// setupCounter(document.querySelector('#counter'))
+
+{
+  /* <div class="flex-none hidden sm:block">
+					<ul class="menu menu-horizontal p-0 text-gray-800">
+						<li>
+							<a href="/">Home</a>
+						</li>
+						<li tabIndex={0}>
+							<a>
+								<a href="/about">About</a>
+								<svg
+									className="fill-current"
+									xmlns="http://www.w3.org/2000/svg"
+									width="20"
+									height="20"
+									viewBox="0 0 24 24"
+								>
+									<path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+								</svg>
+							</a>
+							<ul class="dropdown-content menu p-2 shadow bg-base-200 rounded-box ">
+								<li>
+									<a href="/mualen-tech">Mualen Technology</a>
+								</li>
+								<li>
+									<a href="/out-team">Our Team</a>
+								</li>
+							</ul>
+						</li>
+						<li tabIndex={0} class="dropdown lg:dropdown-hover">
+							<a>
+								<a href="/service">Services</a>
+
+								<svg
+									className="fill-current"
+									xmlns="http://www.w3.org/2000/svg"
+									width="20"
+									height="20"
+									viewBox="0 0 24 24"
+								>
+									<path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+								</svg>
+							</a>
+							<ul
+								tabindex="0"
+								class="dropdown-content menu p-2 shadow bg-base-200 rounded-box "
+							>
+								<li class="capitalize text-sm">
+									<a href="/service">It consulting</a>
+								</li>
+								<li class="capitalize text-sm">
+									<a href="/service">tech start-up advisory</a>
+								</li>
+								<li class="capitalize text-sm">
+									<a href="/service">software development</a>
+								</li>
+								<li class="capitalize text-sm">
+									<a href="/service">web development and ui/ux design</a>
+								</li>
+								<li class="capitalize text-sm">
+									<a href="/service">graphic design</a>
+								</li>
+								<li class="capitalize text-sm">
+									<a href="/service">hardware maintenance and repairs</a>
+								</li>
+								<li class="capitalize text-sm">
+									<a href="/service">Electronics Design/manufacturing</a>
+								</li>
+								<li class="capitalize text-sm">
+									<a href="/service">digital marketing / advertisement</a>
+								</li>
+								<li class="capitalize text-sm">
+									<a href="/service">Mobile app development</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="/products">Products</a>
+						</li>
+						<li>
+							<a href="/faq">FAQ</a>
+						</li>
+					</ul>
+				</div>
+
+				<!-- mobile navigation  -->
+				<div class="hidden">
+					<label tabindex="0" class="btn btn-ghost btn-circle">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							class="inline-block w-5 h-5 stroke-current text-gray-900"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M4 6h16M4 12h16M4 18h16"
+							></path>
+						</svg>
+					</label>
+					<ul
+						tabIndex="0"
+						class="menu menu-compact dropdown-content mt-5 p-3 right-0 shadow bg-gray-900 text-white rounded-box w-56"
+					>
+						<a>
+							<a href="/">Home</a>
+						</li>
+						<li>
+							<a href="/about">About</a>
+						</li>
+						<div tabindex="0" class="m-0 p-0 collapse collapse-arrow">
+							<li class="collapse-title w-full">
+								<a href="/service">Services</a>
+							</li>
+							<div class="collapse-content">
+								<ul>
+									<li class="capitalize text-sm">
+										<a>It consulting</a>
+									</li>
+									<li class="capitalize text-sm">
+										<a>tech start-up advisory</a>
+									</li>
+									<li class="capitalize text-sm">
+										<a>software development</a>
+									</li>
+									<li class="capitalize text-sm">
+										<a>web development and ui/ux design</a>
+									</li>
+									<li class="capitalize text-sm">
+										<a>graphic design</a>
+									</li>
+									<li class="capitalize text-sm">
+										<a>hardware maintenance and repairs</a>
+									</li>
+									<li class="capitalize text-sm">
+										<a>Electronics Design/manufacturing</a>
+									</li>
+									<li class="capitalize text-sm">
+										<a>digital marketing / advertisement</a>
+									</li>
+									<li class="capitalize text-sm">
+										<a>Mobile app development</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<a>
+							<a href="/products">Products</a>
+						</li>
+						<li>
+							<a href="/faq">FAQ</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div> */
+}
